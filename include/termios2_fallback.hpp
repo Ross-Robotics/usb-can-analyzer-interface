bfa53b7b302e62256ca34ff2989370b9e9494270
Pepe <pepe@ross-robotics.co.uk>
2025-04-17 12:51:09 +0100

    Initial commit of V0.1

---
+++ include/termios2_fallback.hpp
@@ -0,0 +1,19 @@
+// termios2_fallback.hpp
+#pragma once
+
+#ifndef TCGETS2
+#define TCGETS2 0x802C542A
+#define TCSETS2 0x402C542B
+#define BOTHER 0010000
+
+struct termios2 {
+  unsigned int c_iflag;
+  unsigned int c_oflag;
+  unsigned int c_cflag;
+  unsigned int c_lflag;
+  unsigned char c_line;
+  unsigned char c_cc[19];
+  unsigned int c_ispeed;
+  unsigned int c_ospeed;
+};
+#endif
